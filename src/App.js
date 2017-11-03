9e6e8158e6a9f8791a25f8b0d632f40e4e9e198f
Adam Friedl <ajfriedl@gmail.com>
2017-11-03 08:36:24 -0400

    removed footer from app

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends Component<Props> {
             <Route path="/bingocard" component={BingoCard} />
           </div>
         </Router>
-        <Footer />
       </div>
     );
   }
