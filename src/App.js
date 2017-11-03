a2621743c4bb19dad0e4539b67537cab57b4580a
Adam Friedl <ajfriedl@gmail.com>
2017-11-03 11:50:46 -0400

    switch to browserrouter with basename

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+// @flow
+
 import React, { Component } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from './Landing';
 import About from './About';
 import BingoCard from './BingoCard';
@@ -10,18 +12,11 @@ class App extends Component<Props> {
   render() {
     return (
       <div>
-        <Router>
+        <Router basename="/lingo-bingo">
           <div>
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + '/'}
-              component={Landing}
-            />
-            <Route path={process.env.PUBLIC_URL + '/about'} component={About} />
-            <Route
-              path={process.env.PUBLIC_URL + '/bingocard'}
-              component={BingoCard}
-            />
+            <Route exact path="/" component={Landing} />
+            <Route path="/about" component={About} />
+            <Route path="/bingocard" component={BingoCard} />
           </div>
         </Router>
       </div>
