5ff5c90ffbb8d84ffbf8c0ef68d24d36f4266ae8
Adam Friedl <ajfriedl@gmail.com>
2017-11-04 14:07:24 -0400

    created customlink for react router

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,20 +2,25 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { Absolute, Toolbar, NavLink } from 'rebass';
+
+const CustomLink = styled(Link)`
+  text-decoration: none;
+  color: #84e47b;
+`;
 
 const NavBar = (props: { color: string }) => {
   return (
     <div>
       <Absolute z={2} top left>
         <Toolbar w={'100%'} bg="transparent" color={props.color}>
-          <Link to="/">
-            <NavLink children="Home" />
-          </Link>
-
-          <Link to="/about">
-            <NavLink color="#84e47b" children="About" />
-          </Link>
+          <NavLink>
+            <CustomLink to="/" children="Home" />
+          </NavLink>
+          <NavLink>
+            <CustomLink to="/about" children="About" />
+          </NavLink>
         </Toolbar>
       </Absolute>
     </div>
