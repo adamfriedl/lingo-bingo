1d143574e64d315b6a65b3f1d6ce65d5163496cf
Adam Friedl <ajfriedl@gmail.com>
2017-11-04 14:07:40 -0400

    made App functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { NavBar } from 'rebass';
 import Landing from './Landing';
 import About from './About';
 import BingoCard from './BingoCard';
 
-type Props = {};
-
-class App extends Component<Props> {
-  render() {
-    return (
-      <div>
-        <Router basename={process.env.PUBLIC_URL}>
-          <div>
-            <Route exact path="/" component={Landing} />
-            <Route path="/about" component={About} />
-            <Route path="/bingocard" component={BingoCard} />
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div>
+          <Route exact path="/" component={Landing} />
+          <Route path="/about" component={About} />
+          <Route path="/bingocard" component={BingoCard} />
+        </div>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
