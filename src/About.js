01c9b317cca560fc347070a86268176299db1355
Adam Friedl <adamfriedl@Adams-MBP-3.fios-router.home>
2018-03-05 21:51:45 -0500

    changed text

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { Container, Heading, Text } from 'rebass';
+import { Container, Heading, Text, Link } from 'rebass';
 import NavBar from './NavBar';
 import Headline from './Headline';
 
@@ -12,10 +12,13 @@ const About = () => (
     <Container>
       <Heading my={[2, 3]} children="About" />
       <Text>
-        Corporate-speak is the worst. If you have to listen to it, the least you
-        can do is have some surreptitious fun at its expense. Play Lingo Bingo
-        during your next conference call or meeting. It might even give you a
-        reason to pay attention.
+        A game inspired by some coworkers seeking relief from the ridiculous
+        corporate-speak we endured on all those conference calls. Unbeknownst to
+        us, the <Link href="https://en.wikipedia.org/wiki/Buzzword_bingo" children="concept" style={{textDecoration: 'none'}} /> has actually existed for quite a while. But that's no
+        reason not to online-ify it!
+        <br /><br />
+        Play Lingo Bingo during your next conference call or meeting. It might
+        even give you a reason to pay attention.
       </Text>
     </Container>
   </div>
